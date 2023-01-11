863ff14b237a54444107de5154ce691a6cae4b11
TJ Momon <tjmomon@TJs-MacBook-Air.local>
2023-01-10 21:34:53 -0600

    added dev notes to script file

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,24 @@
+// selects all time slots in HTML
 var timeSlots = document.querySelectorAll('.time-block');
 
+// sets variable to select the hour from the live clock for comparison in the updateColor function
 var currentHour = dayjs().hour();
 
+// sets variable for the save button(s) in the HTML time blocks
 var saveBtn = document.querySelectorAll('.fas fa-save');
 
+// creates jQuery eventListener for the save button(s). This event saves user input to local storage
 $(".saveBtn").click(function(){
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
   localStorage.setItem(time, text);
 })
 
+// creates live date/time clock at top of page
 var today = dayjs();
 $('#currentDay').text(today.format('MMM D, YYYY h:mm a'));
 
+// function that establishes rules for color-coordination based on timeslot ID compared to current time
 function updateColor () {
    for (i = 0; i < timeSlots.length; i++) {
     var hour = parseInt(timeSlots[i].id);
@@ -26,6 +32,8 @@ function updateColor () {
   }
 };
 
+// calls function above based on setInterval refresh to keep timeclock up-to-date
 updateColor();
 
-setInterval(updateColor, 30000);
+// updates timeclock every 10 seconds to keep clock accurate without having to physically refresh the page
+setInterval(updateColor, 10000);
